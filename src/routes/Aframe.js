2a1cbedc0985e73ff59a964f6f544448c9eb783e
Eva Bustamante <evabrodriguez7@gmail.com>
2023-07-07 12:53:49 +0200

    new proyect

--- src/routes/Aframe.js
+++ src/routes/Aframe.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import 'aframe'
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom';
 
-const Aframe = () => {
-  return (
-    <Link to='/'>HOME</Link>
-  )
+class Basica3D extends Component {
+  render () {
+    return (
+      <a-scene>   
+        <Link to="/">HOME</Link>
+        <a-assets>
+          <img id="skyTexture" src="/assets/background1.jpg" alt="Sky" />
+        </a-assets>
+        <a-sky src="#skyTexture" />
+        <a-entity 
+          geometry="primitive: box" 
+          position="-1 0.5 -3" 
+          rotation="0 45 0" 
+          material="color: #4CC3D9" />
+        <a-entity 
+          geometry="primitive: sphere; radius: 1.25;" 
+          position="0 1.25 -5" 
+          material="color: #EF2D5E" />
+        <a-entity 
+          geometry="primitive: cylinder; radius: 0.5, height: 1.5" 
+          position="1 0.75 -3" 
+          material="color: #FFC65D" />
+          
+      </a-scene>
+
+
+    );
+  }
 }
 
-export default Aframe
+export default Basica3D;
+
+
